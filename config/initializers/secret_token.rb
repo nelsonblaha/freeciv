4b79b441221d41111ce5d2456d747bb154807a7f
nelsonblaha <nelson.blaha@gmail.com>
2013-02-04 20:52:19 -0600

    rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freeciv::Application.config.secret_token = 'ec1dc4ff077b453ec4d6fb6ccac62785c372edc764903fe522cf3d67660d7054be44667d31098b2e5f8ed62a13b12b0ad74fbf9595902eaf03f75876a1c4a64b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Freeciv::Application.config.session_store :cookie_store, key: '_freeciv_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Freeciv::Application.config.session_store :active_record_store
