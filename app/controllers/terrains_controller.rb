0ca7e1857372b9f82fbc770af113e681e77626c8
Nelson Blaha <nelson.blaha@gmail.com>
2013-02-11 23:38:19 -0600

    controller method to cycle through terrain types via REST

--- app/controllers/terrains_controller.rb
+++ app/controllers/terrains_controller.rb
@@ -1,83 +1,101 @@
 class TerrainsController < ApplicationController
-  # GET /terrains
-  # GET /terrains.json
-  def index
-    @terrains = Terrain.all
+  #scaffolding methods
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @terrains }
-    end
-  end
+      # GET /terrains
+      # GET /terrains.json
+      def index
+        @terrains = Terrain.all
 
-  # GET /terrains/1
-  # GET /terrains/1.json
-  def show
-    @terrain = Terrain.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @terrain }
-    end
-  end
-
-  # GET /terrains/new
-  # GET /terrains/new.json
-  def new
-    @terrain = Terrain.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @terrain }
-    end
-  end
-
-  # GET /terrains/1/edit
-  def edit
-    @terrain = Terrain.find(params[:id])
-  end
-
-  # POST /terrains
-  # POST /terrains.json
-  def create
-    @terrain = Terrain.new(params[:terrain])
-
-    respond_to do |format|
-      if @terrain.save
-        format.html { redirect_to @terrain, notice: 'Terrain was successfully created.' }
-        format.json { render json: @terrain, status: :created, location: @terrain }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @terrain.errors, status: :unprocessable_entity }
+        respond_to do |format|
+          format.html # index.html.erb
+          format.json { render json: @terrains }
+        end
       end
-    end
-  end
 
-  # PUT /terrains/1
-  # PUT /terrains/1.json
-  def update
-    @terrain = Terrain.find(params[:id])
+      # GET /terrains/1
+      # GET /terrains/1.json
+      def show
+        @terrain = Terrain.find(params[:id])
 
-    respond_to do |format|
+        respond_to do |format|
+          format.html # show.html.erb
+          format.json { render json: @terrain }
+        end
+      end
+
+      # GET /terrains/new
+      # GET /terrains/new.json
+      def new
+        @terrain = Terrain.new
+
+        respond_to do |format|
+          format.html # new.html.erb
+          format.json { render json: @terrain }
+        end
+      end
+
+      # GET /terrains/1/edit
+      def edit
+        @terrain = Terrain.find(params[:id])
+      end
+
+      # POST /terrains
+      # POST /terrains.json
+      def create
+        @terrain = Terrain.new(params[:terrain])
+
+        respond_to do |format|
+          if @terrain.save
+            format.html { redirect_to @terrain, notice: 'Terrain was successfully created.' }
+            format.json { render json: @terrain, status: :created, location: @terrain }
+          else
+            format.html { render action: "new" }
+            format.json { render json: @terrain.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      # PUT /terrains/1
+      # PUT /terrains/1.json
+      def update
+        @terrain = Terrain.find(params[:id])
+
+        respond_to do |format|
+          if @terrain.update_attributes(params[:terrain])
+            format.html { redirect_to @terrain, notice: 'Terrain was successfully updated.' }
+            format.json { head :no_content }
+          else
+            format.html { render action: "edit" }
+            format.json { render json: @terrain.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      # DELETE /terrains/1
+      # DELETE /terrains/1.json
+      def destroy
+        @terrain = Terrain.find(params[:id])
+        @terrain.destroy
+
+        respond_to do |format|
+          format.html { redirect_to terrains_url }
+          format.json { head :no_content }
+        end
+      end
+
+  def increment_terrain
+      @terrain = Terrain.find(params[:id])
+      last_edited = Terrain.find(params[:last_edited]) if params[:last_edited]
+
+      #TODO set equal to last edited
+
+      #increment the biome but send 10 back around to 0
+      @terrain.biome = (@terrain.biome + 1) % 11
+
       if @terrain.update_attributes(params[:terrain])
-        format.html { redirect_to @terrain, notice: 'Terrain was successfully updated.' }
-        format.json { head :no_content }
+        render json: @terrain.biome
       else
-        format.html { render action: "edit" }
-        format.json { render json: @terrain.errors, status: :unprocessable_entity }
+        render json: @terrain.errors, status: :unprocessable_entity
       end
-    end
-  end
-
-  # DELETE /terrains/1
-  # DELETE /terrains/1.json
-  def destroy
-    @terrain = Terrain.find(params[:id])
-    @terrain.destroy
-
-    respond_to do |format|
-      format.html { redirect_to terrains_url }
-      format.json { head :no_content }
-    end
-  end
+  end 
 end
