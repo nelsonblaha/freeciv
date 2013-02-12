c835f0da44ab15d76c5f4cad2e35783ed5080e3f
Nelson Blaha <nelson.blaha@gmail.com>
2013-02-11 23:39:27 -0600

    biome strings method

--- app/models/terrain.rb
+++ app/models/terrain.rb
@@ -1,32 +1,32 @@
 class Terrain < ActiveRecord::Base
-  attr_accessible :city_id, :civ_id, :lat, :lng, :biome
+  attr_accessible :city_id, :civ_id, :lat, :lng, :biome, :map_id
 
   def biome_string
-  	case self.biome
-    	  when 0
-    	   "glacier"
-        when 1
-          "desert"
-        when 2
-          "forest"
-        when 3
-          "grassland"
-        when 4
-          "hills"
-        when 5
-          "jungle"
-        when 6
-          "mountains"
-        when 7
-          "ocean"
-        when 8
-          "plains"
-        when 9
-          "swamp"
-        when 10
-          "tundra"
-        else
-          "ocean"
-    	end
+      	case self.biome
+                when 0
+            	   "glacier"
+                when 1
+                  "desert"
+                when 2
+                  "forest"
+                when 3
+                  "grassland"
+                when 4
+                  "hills"
+                when 5
+                  "jungle"
+                when 6
+                  "mountains"
+                when 7
+                  "ocean"
+                when 8
+                  "plains"
+                when 9
+                  "swamp"
+                when 10
+                  "tundra"
+                else
+                  "ocean"
+        	end
   end
 end
