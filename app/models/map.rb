b73e93a070cca6fd9aadbbf879806645acce59a4
Nelson Blaha <nelson.blaha@gmail.com>
2013-02-11 23:39:01 -0600

    after_create to populate maps with a terrain object for each square

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,3 +1,13 @@
 class Map < ActiveRecord::Base
   attr_accessible :width, :height
+
+  has_many :terrains
+
+  after_create :make_terrain
+
+  def make_terrain
+    (self.width * self.height).times do
+      self.terrains.create()
+    end
+  end
 end
