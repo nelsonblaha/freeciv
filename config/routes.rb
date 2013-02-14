420545e9a781a51d6659f59ab8b911577b574e5b
nelsonblaha <nelson.blaha@gmail.com>
2013-02-13 19:32:55 -0600

    remove map controller (using 'maps' now)

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Freeciv::Application.routes.draw do
 
   resources :maps
 
-
-  get "map/index"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -56,7 +53,7 @@ Freeciv::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'map#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
